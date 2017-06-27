4a3d98c9a2e89e4f7e8544dee051d11b19948375
vagrant <vagrant@vagrant.vm>
2017-06-27 03:20:29 +0000

    Set API call to every ten pages. Saved output to text file.

--- api.py
+++ api.py
@@ -3,39 +3,37 @@ import os
 import sys
 
 
-# beer_results = []
-# per notes from henry, make each request a seperate line in the output file. 
 f = open("beers.txt", "w")
 
-for i in range(5, 9):
+for i in range(1, 1264):
+    if i % 10 == 0:
 
-    APIKEY = ''
-    # APIKEY =os.environ['API_KEY']
-    # payload = {'withBreweries': 'Y', 'withIngredients': 'Y', 'p': i}
-    payload = {'withIngredients': 'Y', 'p': i}
+        APIKEY = '9726819debab5cfdba5b6744dbbf1616'
+        # APIKEY =os.environ['API_KEY']
+        # payload = {'withBreweries': 'Y', 'withIngredients': 'Y', 'p': i}
+        payload = {'p': i}
 
-    response = requests.get('http://api.brewerydb.com/v2/beers?key='+APIKEY, params=payload)
+        response = requests.get('http://api.brewerydb.com/v2/beers?key='+APIKEY, params=payload)
 
-    print(response.url)
-    #print it just for myself to make sure it is the correct url!
 
-    # write each response as a new line in beers.txt    
-    f.write('> %s\n' % (response,))
-    # http://www.afterhoursprogramming.com/tutorial/Python/Writing-to-Files/
-    # http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
-    # put beer_raw in to file and add a new line character
+        print(response.url)
+        #print it just for myself to make sure it is the correct url!
+
+
+        beer_raw = response.json()
+
+
+         
+        f.write('> %s\n' % (beer_raw,))
+        # write each response as a new line in beers.txt  
+        # http://www.afterhoursprogramming.com/tutorial/Python/Writing-to-Files/
+        # http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+
 
 f.close()
 
+------------------------------------------------------------
 
-beer_raw = response.json()
-    #I think I need it to be a string in order to write it. so I will not make a json until I have all of the lines
-
-
-    # beer_results.append(beer_raw)
-
-
-# need to replace indexes with the current number
 for i in range(0, 49):
 
     name = beer_raw['data'][i].get('name')
@@ -53,10 +51,21 @@ for i in range(0, 49):
 
     print name, abv
 
+------------------------------------------------------------
 
+def process_beers():
+    """Read beers file and return dictionary of {restaurant-name: score}."""
 
+    beers_txt = open('beers.txt')
 
-1294 
+    beers = {}
+
+    for line in beers_txt:
+        line = line.rstrip()
+        restaurant, score = line.split(":")
+        scores[restaurant] = int(score)
+
+    return scores
  
 
 # do a file write, add new line character add the end. 
