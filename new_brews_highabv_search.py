e882c2bb90e88648f6eeb0b78c67ac96d003240d
vagrant <vagrant@vagrant.vm>
2017-07-04 23:22:30 +0000

    Added search for the latest high abv brews. For use in StoutSpotlight route.

---
+++ new_brews_highabv_search.py
@@ -0,0 +1,32 @@
+"""Get latest feature from BDB."""
+
+import requests
+import json
+
+r = requests.get("http://api.brewerydb.com/v2/beers?key=9726819debab5cfdba5b6744dbbf1616&order=createDate&sort=DESC&abv=+10")
+
+#Get the latest beer realeases with an abv over 10
+
+new_brews = r.json()
+
+for i in range(0, 50):
+
+    name = new_brews['data'][i].get('name')
+    beer_id = new_brews['data'][i].get('id')
+    description = new_brews['data'][i].get('description')
+    name_display = new_brews['data'][i].get('nameDisplay')
+    labels = new_brews['data'][i].get('labels')
+    website = new_brews['data'][i].get('website')
+    year = new_brews['data'][i].get('year')
+    beer_variation = new_brews['data'][i].get('beerVariation')
+    beer_variation_id = new_brews['data'][i].get('beerVariationId')
+    style_id = new_brews['data'][i].get('styleId')
+    abv = new_brews['data'][i].get('abv')
+    ibu = new_brews['data'][i].get('ibu')
+
+    if description != None:
+
+        print name 
+        print description
+
+           
