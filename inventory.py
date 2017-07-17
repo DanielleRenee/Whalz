c91705b88170aee744570553bf7b6c0a81cc6cc3
vagrant <vagrant@vagrant.vm>
2017-07-17 23:53:44 +0000

    Added docstring tests for PSQL inventory and iso queries.

--- inventory.py
+++ inventory.py
@@ -1,4 +1,12 @@
 def show_user_inventory(user_id):
+    """ 
+    Query PSQL database and make and print a list of all a user's inventory.
+
+    >>> psql whalz
+    >>> SELECT * FROM inventories WHERE user_id = 44;
+
+
+    """
     user = User.query.get(user_id)
     inventory = Inventory.query.filter_by(user_id=user.user_id).all()
 
@@ -10,11 +18,19 @@ show_user_inventory(3)
 
 
 def show_user_iso(user_id):
+    """
+    Query PSQL database and make and print a list of all a user's isos.
+
+    >>> psql whalz
+    >>> SELECT * FROM iso WHERE user_id = 64;
+
+    """
     user = User.query.get(user_id)
     iso = ISO.query.filter_by(user_id=user.user_id).all()
 
     for i in iso:
         print i.beer.name
+
 
 show_user_inventory(3)
 
