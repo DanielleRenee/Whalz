453d91f227e70d63d0cbaa3998c743e10a3e388a
vagrant <vagrant@vagrant.vm>
2017-07-17 19:37:11 +0000

    Found bug where abv was not listed in new brewery release. Added conditional statement before appending to new_description list.

--- server.py
+++ server.py
@@ -30,15 +30,6 @@ def index():
     return render_template("homepage.html")
 
 
-
-
-# @app.route('/testingd3')
-# def d3():
-#     """Testing d3."""
-
-#     return render_template("xindex.html")
-
-
 @app.route("/trials")
 def trials():
     """Return trials."""
@@ -51,13 +42,6 @@ def example():
     return render_template("index_too.html")
 
 
-# @app.route('/basic')
-# def basic():
-#     """Testing plain d3."""
-
-#     return render_template("basicd3.html")
-
-
 
 @app.route('/users')
 def user_list():
@@ -65,7 +49,6 @@ def user_list():
 
     users = User.query.all()
     return render_template("user_list.html", users=users)
-
 
 
 
@@ -211,9 +194,11 @@ def fresh():
             style_id = new_brews['data'][i].get('styleId')
             abv = new_brews['data'][i].get('abv')
             ibu = new_brews['data'][i].get('ibu')
-            abv_display_bar = float(abv) * 10
+            
+            if abv != None: 
+                abv_display_bar = float(abv) * 10
 
-            if new_description != None:
+            if new_description != None and abv != None:
                     new_list.append([new_name, new_description, new_brewery_name, abv, abv_display_bar])
 
     # print new_list
@@ -223,137 +208,6 @@ def fresh():
                             new_list=new_list)
 
 
-
-
-
-def make_nodes_and_links():
-    
-    brews = []
-
-    for i in range(1200, 1202):
-        # if i % 2 == 0:
-
-            APIKEY = '9726819debab5cfdba5b6744dbbf1616'
-            # APIKEY =os.environ['API_KEY']
-            payload = {'withBreweries': 'Y', 'withIngredients': 'Y', 'p': i}
-            # payload = {'p': i}
-
-            b = requests.get('http://api.brewerydb.com/v2/beers?key='+APIKEY, params=payload)
-
-
-            print(b.url)
-
-            #print it just for myself to make sure it is the correct url!
-
-            brew_list = b.json()
-
-            brews.append(brew_list)
-
-
-    #create a list of tuples with brewey name, beer name, and beer style_id
-    # style_tuples = []
-
-
-    # beer_dict = {}
-
-    d3 = {}
-    d3['nodes'] = []
-    d3['paths'] = []
-
-
-    # for url in range(len(brews)):
-    x = range(len(brews))
-
-    for i in x:
-
-        for y in range(0, 49):
-
-            # brewery = brews['data'][x].get('breweries')
-
-            brewery = brews[i]['data'][y].get('breweries')
-
-            if brewery != None:
-
-                brewery_name = brewery[0]['name']
-                # brewery_local = brewery[0]['locations'][y]['region']
-                brewery_id = brewery[0]['id']
-                # brewery_type = brewery[0]['locations'][0]['locationTypeDisplay']
-                # brewery_website = brewery[0]['website']
-                # brewery_established = brewery[0]['established']
-                # brewery_open = brewery[0]['locations'][0]['yearOpened']
-
-                # example: brewery_name = brewery[0]['name']
-                #          u'Harpoon Brewery'
-
-                sty_num = brews[i]['data'][y].get('styleId')
-
-                beer_name = brews[i]['data'][y].get('name')
-               
-                # example: sty_num = brews['data'][1].get('styleId')
-                #          61
-
-
-                # check to see if brewery_name is in dictionary. if it is not, add it. 
-                # if brewery_name in beer_dict:
-                #     beer_dict[brewery_name].append((beer_name, sty_num))
-
-                # else:
-                #     beer_dict[brewery_name] = [(beer_name, sty_num)]
-
-
-                if brewery_name in d3['nodes']:
-                    d3['paths'].append({'source': brewery_name, 'target': sty_num, "beer": beer_name})
-                 
-                else:
-                    d3['nodes'].append({'name': brewery_name, "id": brewery_id})
-                    d3['paths'].append({'source': brewery_name, 'target': sty_num, "beer": beer_name})
-                    
-    return d3
-
-
-
-
-
-
-# def charts():
-#     """Ideas for Charts.js."""
-# highest secondary market beer price
-
-
-# name, and the price
-
-
-# def whalz_watch():
-#     """Display a featured beer and brewery. Spotlight feature."""
-
-
-
-# def fresh_off_the_boat():
-#      """Show the freshest newest hitting right now."""
-
-
-
-
-# def get_out_there():
-#     """List upcoming beer trade oppoprtunitues."""
-
-#        http://api.brewerydb.com/v2/events?key=9726819debab5cfdba5b6744dbbf1616
-
-
-
-
-# def real_whalz():
-#     """Display opportunity to trade for one of the rarest beers. Less than 5 qty across the whole database."""
-
-#     #this will come from internal db
-
-
-
-
-# def trades_in_numbers():
-#     """Display hot comodities. Beers that are trading at very high rates."""
-
-#     #this will come from internal db
 
 
 
